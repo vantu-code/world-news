c415035e5f7f35f3e277212e9bbc13001aec55a4
Jonathan <jonathanvantu@gmail.com>
2019-11-27 11:13:12 +0100

    took styles

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,7 @@ router.use('/signup', signupRouter);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'One News' });
+  res.render('auth-views/login');
 });
 
 module.exports = router;
